93ff332bc704b87537e5b9a48538de248c1de05b
CJX <9thflr.cjx@gmail.com>
2017-10-24 17:08:12 +0800

    add sign in/ sign up

--- assets/js/firebaseConfig.js
+++ assets/js/firebaseConfig.js
@@ -1,12 +1,23 @@
+// // Initialize Firebase
+
+// var config = {
+// 	apiKey: "AIzaSyALpNZ4uw4cAt74i14ka8MYmlZxQTHR97k",
+// 	authDomain: "tinichats-53c3e.firebaseapp.com",
+// 	databaseURL: "https://tinichats-53c3e.firebaseio.com",
+// 	projectId: "tinichats-53c3e",
+// 	storageBucket: "tinichats-53c3e.appspot.com",
+// 	messagingSenderId: "982360403003"
+// };
+
+// firebase.initializeApp(config);
+
 // Initialize Firebase
-
 var config = {
-	apiKey: "AIzaSyALpNZ4uw4cAt74i14ka8MYmlZxQTHR97k",
-	authDomain: "tinichats-53c3e.firebaseapp.com",
-	databaseURL: "https://tinichats-53c3e.firebaseio.com",
-	projectId: "tinichats-53c3e",
-	storageBucket: "tinichats-53c3e.appspot.com",
-	messagingSenderId: "982360403003"
+	apiKey: "AIzaSyCvEeDhpi-a8TDq5xzf3bG0GLyeHmyWz1c",
+	authDomain: "tinichats-1c78f.firebaseapp.com",
+	databaseURL: "https://tinichats-1c78f.firebaseio.com",
+	projectId: "tinichats-1c78f",
+	storageBucket: "tinichats-1c78f.appspot.com",
+	messagingSenderId: "703410504817"
 };
-
 firebase.initializeApp(config);

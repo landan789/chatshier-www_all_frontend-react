76e6754d44c0023f92cf9150bd34fc256f9eb65b
HTH <9thflr.tzuhsinhsu@gmail.com>
2017-11-16 07:47:16 +0000

    Merged in feature/config (pull request #21)
    [feature/config] [更改 chatshier url 與 chatshier db 設定檔案]

--- config/firebase-config-dev.js
+++ config/firebase-config-dev.js
@@ -1,8 +1,8 @@
-var config = {
-	apiKey: "AIzaSyDl8TXlzJ_Nu1mN5uzxHXfGZ9gj_e-HcJo",
-	authDomain: "chat-7c524.firebaseapp.com",
-	databaseURL: "https://chat-7c524.firebaseio.com",
-	projectId: "chat-7c524",
-	storageBucket: "chat-7c524.appspot.com",
-	messagingSenderId: "369862089268"
-};
+  var config = {
+    apiKey: "AIzaSyCjZDIYFmILpcbhmHpRtSBOFyEASEvj1N0",
+    authDomain: "dev-chatshier.firebaseapp.com",
+    databaseURL: "https://dev-chatshier.firebaseio.com",
+    projectId: "dev-chatshier",
+    storageBucket: "dev-chatshier.appspot.com",
+    messagingSenderId: "635774265645"
+  };

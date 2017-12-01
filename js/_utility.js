24df49d2215f7703727b4d40c3ab85a7b63ed4c5
HTH <9thflr.tzuhsinhsu@gmail.com>
2017-12-01 09:47:11 +0000

    Merged in feature/navbar (pull request #67)
    Feature/navbar
    
    * [feature/navbar] [navbar css 微調]
    
    * [feature/navbar] [修正 cookie 於 fea.chatshier.com ]
    
    * [feature/navbar] [調整 navbar 使用 glyphicon]

--- js/_utility.js
+++ js/_utility.js
@@ -1,14 +1,12 @@
-function getCookie(cName){
-  if (document.cookie.length>0)
-    {
-    cStart=document.cookie.indexOf(cName + "=")
-    if (cStart!=-1)
-      {
-        cStart=cStart + cName.length+1
-        cEnd=document.cookie.indexOf(";",cStart)
-        if (cEnd==-1) cEnd=document.cookie.length
-        return unescape(document.cookie.substring(cStart,cEnd))
-      }
+function getCookie(cName) {
+    if (document.cookie.length > 0) {
+        cStart = document.cookie.indexOf(cName + "=")
+        if (cStart != -1) {
+            cStart = cStart + cName.length + 1
+            cEnd = document.cookie.indexOf(";", cStart)
+            if (cEnd == -1) cEnd = document.cookie.length
+            return unescape(document.cookie.substring(cStart, cEnd))
+        }
     }
-  return ""
+    return ""
 }

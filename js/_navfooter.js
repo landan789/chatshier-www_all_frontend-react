20ceedab16bfea0907a75a46702d44528eefc039
WYA <9thflr.wuyuan@gmail.com>
2017-12-01 15:15:46 +0800

    所有頁面的navbar、footer整合

--- js/_navfooter.js
+++ js/_navfooter.js
@@ -16,7 +16,7 @@ var email = getCookie('email');
 
 if( '' !== name && '' !== email){
   $('#login').addClass('hidden');
-  $('#signup').addClass('hidden');
+  $('#nav-signup').addClass('hidden');
   $('#chat').removeClass('hidden');
   $('#dropdown-user').removeClass('hidden');
   $('#name').html('');
@@ -26,13 +26,14 @@ if( '' !== name && '' !== email){
 
 }else{
   $('#login').removeClass('hidden');
-  $('#signup').removeClass('hidden');
+  $('#nav-signup').removeClass('hidden');
   $('#chat').addClass('hidden');
   $('#dropdown-user').addClass('hidden');
 
 }
 $('#login').attr('href', loginUrl);
-$('#signup').attr('href', signupUrl);
+$('#nav-signup').attr('href', signupUrl);
+$('#footer-signup').attr('href', signupUrl); /*footer的馬上註冊button*/
 $('#chat').attr('href', chatUrl);
 
 

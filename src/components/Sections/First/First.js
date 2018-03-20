7fd9f56c6b260e56f5c1e85a0f66e9373e65be8c
KCY <9thflr.liv@gmail.com>
2018-03-20 17:03:16 +0800

    [Desktop] Header and banner

--- src/components/Sections/First/First.js
+++ src/components/Sections/First/First.js
@@ -11,10 +11,11 @@ const First = (props) => {
                     <Col>
                         <h1>Chatshier</h1>
                         <p>Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. </p>
+                        <Button className="desktopShow" color="info">CTA</Button>
                     </Col>
                 </Row>
             </Container>
-            <Button color="info">CTA</Button>
+            <Button className="desktopHide" color="info">CTA</Button>
         </Fade>
     );
 };

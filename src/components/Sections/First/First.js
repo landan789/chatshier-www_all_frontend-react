c12527a3788de9aa9affdd1277e81f03070bd868
KCY <9thflr.liv@gmail.com>
2018-03-27 13:51:40 +0800

    Styling: refining responsive and applying theme color

--- src/components/Sections/First/First.js
+++ src/components/Sections/First/First.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Fade, Container, Row, Col, Button } from 'reactstrap';
+import { Fade, Button } from 'reactstrap';
 
 import './First.css';
-
+import homeBanner from '../../../assets/images/banners/homebanner-by-rawpixel.jpg';
 const First = (props) => {
     return (
-        <Fade className="First" style={{backgroundImage: 'url(' + 'https://unsplash.it/1400/799' + ')'}}>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>Chatshier</h1>
-                        <p>數據驅動策略<br/>優化客服體驗</p>
-                        <Button outline className="btndesktopShow" color="info">免費體驗</Button>
-                    </Col>
-                </Row>
-            </Container>
-            <Button className="btndesktopHide" color="info">CTA</Button>
+        <Fade className="First" style={{backgroundImage: 'url(' + homeBanner + ')'}}>
+            <div className="content">
+                <h1>Chatshier</h1>
+                <p>數據驅動策略<br/>優化客服體驗</p>
+                <Button outline className="btndesktopShow" color="info">免費體驗</Button>
+                <Button className="btndesktopHide" color="info">免費體驗</Button>
+            </div>
+            <div className="mask"></div>
         </Fade>
     );
 };

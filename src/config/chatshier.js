31168cb01878c3216136bc8734527de4d4986ff0
TED <hwanghsin@9thflr.com>
2018-06-26 11:19:54 +0800

    新增GA dev跟rel的代碼

--- src/config/chatshier.js
+++ src/config/chatshier.js
@@ -6,6 +6,14 @@ const CHATSHIER = {
     GA: {
         CHATSHIER: 'UA-121345606-1',
         DSDSDS: 'UA-121307398-1'
+    },
+    devGA: {
+        CHATSHIER: 'UA-121446124-1',
+        DSDSDS: 'UA-121426051-1'
+    },
+    relGA: {
+        CHATSHIER: 'UA-121420511-1',
+        DSDSDS: 'UA-121421810-1'
     }
 }
 

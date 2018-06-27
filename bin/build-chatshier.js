270abe65f5f9d4ef7e4e243c9325b686cf74f13e
TED <hwanghsin@9thflr.com>
2018-06-27 16:41:11 +0800

    build html for Chatshier and DSDSDS

---
+++ bin/build-chatshier.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var createHTML = require('create-html');
+
+return Promise.all([
+    fs.readFileSync('./public/head-chatshier.html', {encoding: 'utf8'}),
+    fs.readFileSync('./public/body.html', {encoding: 'utf8'})
+]).then((readFileOutput) => {
+    let head = readFileOutput[0];
+    let body = readFileOutput[1];
+    var html = createHTML({ title: '專業客服整合平台', head, body });
+    fs.writeFileSync('./public/index.html', html, {encoding: 'utf8'});
+}).catch((ERR) => {
+    console.log(ERR);
+});
